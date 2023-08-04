35afc503cb4b5fff7583e4c127a0039340a49392
Gaurpad Shukla <gaurpadshukla@gmail.com>
2023-08-04 22:43:22 +0530

    Add files via upload

---
+++ files.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "CP guy", "philantrophist",
+        "Gamer", "Techy guy", "GEEK"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    Loop: true
+})
+
+let sections = document.querySelector('section');
+let navlinks = document.querySelector('header nav a');
